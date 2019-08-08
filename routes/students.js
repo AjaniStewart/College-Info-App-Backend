363d2a82ede8427cdcfca46eceb6cdf091e91348
azheng4119 <azheng4119@bths.edu>
2019-08-08 14:37:27 -0400

    Student endpoints

--- routes/students.js
+++ routes/students.js
@@ -1,47 +1,84 @@
 let model = require('../database/models/student')
 const router = require("express").Router();
 
+
+  //api/students/  gets all students
   router.get('/', async (req, res, next) => {
-    // let currentStudent = {
-    //   "id": 4,
-    //   "firstName": "Jerry",
-    //   "lastName": "Jingle",
-    //   "email": "jerryjingle@bells.com",
-    //   "imageUrl": "http://i.imgur.com/AItCxSs.jpg",
-    //   "gpa": 1.0,
-    //   "CampusId": 3
-    //   };
-    //let builtStudent = await model.build(currentStudent)
-    // const builtstudent = await model.create(currentStudent).then(console.log('ewqeqw')).catch(function(error) {
-    //   console.log(error);
-    // });
-    res.json(await model.findAll().catch(function(error){
+    let foundStudents = await model.findAll().catch(
+      function(error){
+        console.log(error);
+      }
+    );
+    if (foundStudents) res.json(foundStudents);
+    else{
+      res.status(404).send(`No students found`);
+    }
+  });
+
+  ///api/students/id - gets student with id
+  router.get('/:id', async (req, res, next) => {
+    let num = req.params.id;
+    foundStudent = await model.findOne({
+      where: {
+        id : num
+      }
+    }).catch(function(error){
       console.log(error);
-    }));
+    });
+    if (foundStudent) res.status(200).send(foundStudent);
+    else{
+      res.status(404).send(`No student with id ${num} found`);
+    }
   });
   
-  router.get('/:id', (req, res, next) => {
-    let num = req.params.id;
-    res.status(200).send(students[num]);
-  });
-  
-  // router.post('/', (req, res, next) => {
-  //   let newStudent = {'hello':'salmdl'};
-  //   students.push(newStudent);
-  //   res.json({'code':'userCreated'});
-  // });
-
-  router.delete('/:id', (req, res, next) => {
-    let num = req.params.id;
-    students.splice(num, 1); 
-   res.json({'code':'userDeleted'});
+  router.post('/', async(req, res, next) => {
+    const student = req.body;
+    console.log(student);
+    let builtStudent = await model.create(student).catch(error =>{
+      console.log(error);
+    });
+    if (builtStudent) res.status(200).send(builtStudent)
+    else{
+      res.status(404).send("Invalid student info");
+    }
   });
 
-  router.put('/:id', (req, res, next) => {
+  router.delete('/:id', async (req, res, next) => {
+    let num = req.params.id;
+    const numberDeleted = await model.destroy({
+      where: {
+        id : num
+      }
+    }).catch(error =>{
+      console.log(error);
+    });
+    if (numberDeleted != 0) res.status(200).send(`Deleted ${numberDeleted} number of rows with id ${num}.`)
+    else{
+      res.status(404).send(`Did not find any students with id ${num} to delete. `)
+    }
+  });
+
+  router.put('/:id', async (req, res, next) => {
     let modifiedStudent = req.body;
     let num = req.params.id;
-    students[num] = modifiedStudent;
-    res.status(200);
+    let modified = await model.update(
+    {
+      "firstName": modifiedStudent.firstName,
+      "lastName": modifiedStudent.lastName,
+      "email": modifiedStudent.emailName,
+      "imageUrl": modifiedStudent.imageUrl,
+      "gpa": modifiedStudent.gpa,
+      "CampusId": modifiedStudent.CampusId
+    }, 
+    {
+      where : {
+        id : num
+      }
+    }).catch(error=>{console.log(error)});
+    if(modified) res.status(200).send(`Updated student with id ${num}`);
+    else{
+      res.status(404).send(`did not find student`);
+    }
   });
 
   module.exports = router;
